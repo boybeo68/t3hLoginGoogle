cf83c8b79eae529fb5ead85679dad758797380ed
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2021-03-07 15:37:06 +0700

    config notification android and íó

--- App.js
+++ App.js
@@ -1,17 +1,62 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { useEffect } from 'react';
+import { Button, Text, View } from 'react-native';
+import { fcmService } from './service/FCMservices';
+import { localNotificationService } from './service/localNotificationService';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const App = ({
+  params,
+}) => {
+  useEffect(() => {
+    fcmService.requestUserPermission();
+    fcmService.registerAppWithFCM();
+    fcmService.register(onRegister, onNotification, onOpenNotification);
+    localNotificationService.config(onOpenNotification);
 
-  render() {
-    return (
-      <View>
-        <Text> App </Text>
-      </View>
-    );
-  }
+    function onRegister(token) {
+      console.log('[App] onRegister : ', token);
+    }
+    function onNotification(notify) {
+      console.log('[App] onNotification : ', notify);
+      Alert.alert(
+        notify.title,
+        notify.body,
+        [
+          {
+            text: 'Cancel',
+            onPress: () => console.log('Cancel Pressed'),
+            style: 'cancel',
+          },
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ],
+        { cancelable: false },
+      );
+      const options = {
+        soundName: 'default',
+        playSound: true,
+      };
+      localNotificationService.showNotification(
+        0,
+        notify.title,
+        notify.body,
+        notify,
+        options,
+      );
+    }
+    function onOpenNotification(notify) {
+      console.log('[App] onOpenNotification : ', notify);
+      // alert('[App] onOpenNotification : ', notify);
+    }
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
+      <Button onPress={() => {
+        localNotificationService.showNotification(
+          1, "App notification", "Local notification", {}, {},
+        )
+      }} title={'send local notificaiton'} />
+    </View>
+  );
 }
+
+export default App;
--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-
+import { name as appName } from './app.json';
+import { fcmService } from './service/FCMservices';
+AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => fcmService.registerHandlessTaskClose)
 AppRegistry.registerComponent(appName, () => App);
